9850ccb1848c28b48d4fe6bedaa1fd7f9e3a476d
Anastasiya Yegorova <anast4si4a@gmail.com>
2025-12-09 19:56:18 +0300

    Add files via upload

---
+++ homework_1/src/edu/training/jc/simple_output/SimpleConsoleOutput.java
@@ -0,0 +1,8 @@
+package edu.training.jc.simple_output;
+
+public class SimpleConsoleOutput {
+	 public static void main(String[] args) {
+	 System.out.println("A\nB\nC\nD");}
+	}
+
+
---
+++ homework_1/src/edu/training/jc/simple_output/TableOutput.java
@@ -0,0 +1,11 @@
+package edu.training.jc.simple_output;
+
+public class TableOutput {
+public static void main (String[] args) {
+	System.out.println("---------------------------------");
+	System.out.println("|\tid\t|\trole\t|");
+	System.out.println("---------------------------------");
+	System.out.println("|\t1\t|\tguest\t|\n|\t10\t|\tuser\t|\n|\t100\t|\tadmin\t|");
+	System.out.println("---------------------------------");
+}
+}
